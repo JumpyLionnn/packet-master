9c3f1f56f3311ccb25bdf5ded5e6d68c7c7408c5
JumpyLionnn <jumpylionn1@gmail.com>
2024-02-03 18:02:53 +0200

    Add tests boilerplate

--- tests/tests.c
+++ tests/tests.c
@@ -1,7 +1,31 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <packet_master.h>
 
-int main() {
-    printf("Hello, World! %i \n", value());
-    return 0;
+int expect_int_eq(int a, int b) {
+    if (a == b) {
+        return 0;
+    }
+    else {
+        printf("Test failed: expected %i == %i.\n", a, b);
+        return 1;
+    }
+}
+
+int main()
+{
+    int failed = 0;
+
+    failed += expect_int_eq(value(), 2);
+
+    if (failed == 0)
+    {
+        printf("All tests passed\n");
+        return EXIT_SUCCESS;
+    }
+    else
+    {
+        printf("%i tests failed\n", failed);
+        return EXIT_FAILURE;
+    }
 }
